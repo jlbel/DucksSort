b675d8d59158bac74198da02d13f0623f45a940e
Julia Belyaeva <julia.belyaeva2012@gmail.com>
2023-06-27 11:32:31 +0300

    Добавьте файлы проекта.

---
+++ Ducks/Duck.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ducks
+{
+     class Duck: IComparable<Duck>
+    {
+        public override string ToString()
+        {
+            return $"A {Size} inch {Kind}";
+        }
+        public int Size { get; set; }
+        public KindOfDuck Kind { get; set; }
+        public int CompareTo(Duck duckToComprate)
+        {
+            if (this.Size > duckToComprate.Size) return 1;
+            else if (this.Size < duckToComprate.Size) return -1;
+            else return 0;
+        }
+    }
+   
+    enum KindOfDuck
+    {
+        Mallard,
+        Muscovy,
+        Loon,
+    }
+
+}
